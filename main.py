3b35a60194ea5878411c02e41ac9b4270253fb82
Fer Rodríguez <ferrodriguez2509@gmail.com>
2025-05-12 04:45:37 -0600

    test: prueba de la version 0.1.6 desde el testpypi

--- main.py
+++ main.py
@@ -1,37 +1,34 @@
-# %%
+# %% 
 import enerhabitat as eh 
 import plotly.express as px
-# %%
 
-dia = eh.calculateTSA(
-    convection_heat_transfer=13, ## por default 
-    solar_absortance=0.8,
-    inclination=90, 
-    azimuth=90,
-    month="04",
-    year="2025", ##obligatorio por default
-    epw_file_path="epw/USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw"
-    )
 # %%
-px.scatter(
-    data_frame=dia,
-    x=dia.index,
-    y=["Ta","Tsa"]
-)
+eh.materials('./eh_config/doblecarpeta/materiales_nuevos.ini')
+
+#%%
+dia = eh.Tsa(
+    solar_absortance=0.8,
+    surface_tilt=90, 
+    surface_azimuth=90,
+    month="04",
+    year="2025",
+    epw_file="epw/USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw"
+    )
+
 #%%
 sc = [
-    (0.001,"acero"),
-    # (0.1, "adobe")
-#     (0.02, "tabique"),
-#     (0.1, "concreto"),
+    (0.001,"steel"),
+#    (0.1, "adobe")
+#    (0.02, "brick"),
+#    (0.1, "concrete"),
 ]
 
-dia = eh.solveSC(sc, dia)
+dia = eh.solveCS(sc, dia)
 
 #%%
-
 px.scatter(
     data_frame=dia,
     x=dia.index,
     y=["Ta","Tsa","Ti"]
 )
+# %%
