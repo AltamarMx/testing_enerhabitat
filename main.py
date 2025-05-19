698d3729e811f122bfc8b280ee2a7d1d70cbb79d
altamarmx <gbv@ier.unam.mx>
2025-05-19 16:18:00 -0600

    testing one SC

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 # %% 
 import enerhabitat as eh 
+from matplotlib.pylab import e
 import plotly.express as px
-# %%
 
-dia = eh.calculateTsa(
-    convection_heat_transfer=13, ## por default 
-    solar_absortance=0.8,
-    inclination=90, 
-    azimuth=90,
-    month="04",
-    year="2025", ##obligatorio por default
-    epw_file_path="epw/USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw"
-    )
 # %%
+eh.get_list_materials()
+# %%
+eh.Nx = 200
+sc1 = eh.Tsa(
+    solar_absortance=0.8,
+    surface_tilt=90, 
+    surface_azimuth=90,
+    month="04",
+    epw_file="epw/USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw"
+    )
+
+sc = [
+    (0.3,"adobe")
+]
+
+sc1 = eh.solveCS(sc,sc1)
+
+
 px.scatter(
-    data_frame=dia,
-    x=dia.index,
-    y=["Ta","Tsa"]
+    data_frame=sc1,
+    x=sc1.index,
+    y=["Ta","Ti"]
 )
+
+# %%
